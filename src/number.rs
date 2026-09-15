636ec1919d3631490141dd31e5e676d93aaff489
Markus Ineichen <kusi.ineichen@gmail.com>
2026-09-15 09:15:05 +0200

    Add bidirectionality between Sqrtable and Squareable

--- src/number.rs
+++ src/number.rs
@@ -1,4 +1,7 @@
-pub trait Squareable: TryFrom<Self::Square> {
+pub trait Squareable: TryFrom<Self::Square>
+where
+    Self::Square: Sqrtable<Sqrt = Self>,
+{
     type Square: From<Self>;
 }
 
@@ -11,4 +14,31 @@ impl Squareable for u16 {
 }
 impl Squareable for u32 {
     type Square = u64;
+}
+
+impl Squareable for u64 {
+    type Square = u128;
+}
+
+pub trait Sqrtable: Sized + From<Self::Sqrt>
+where
+    Self::Sqrt: Squareable<Square = Self>,
+{
+    type Sqrt: TryFrom<Self>;
+}
+
+impl Sqrtable for u16 {
+    type Sqrt = u8;
+}
+
+impl Sqrtable for u32 {
+    type Sqrt = u16;
+}
+
+impl Sqrtable for u64 {
+    type Sqrt = u32;
+}
+
+impl Sqrtable for u128 {
+    type Sqrt = u64;
 }
